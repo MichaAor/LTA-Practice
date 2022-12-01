28927788c3da002e33a3b8c0ac57d98de681a98b
Bluckiz <57368872+MichaAor@users.noreply.github.com>
2022-12-01 20:07:41 -0300

    Estructura Establecida, Pract6

---
+++ 6. Desarrollo Relaciones Spring Boot con Thymeleaf + MySQL + Hibernate y JUnit/InvManag-app/src/main/java/com/lta/invmanagapp/InvManagAppApplication.java
@@ -0,0 +1,13 @@
+package com.lta.invmanagapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvManagAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvManagAppApplication.class, args);
+	}
+
+}
---
+++ 6. Desarrollo Relaciones Spring Boot con Thymeleaf + MySQL + Hibernate y JUnit/InvManag-app/src/test/java/com/lta/invmanagapp/InvManagAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lta.invmanagapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvManagAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
